810466f7a5cc5bde39e401e4d53dd7cd5c47dd84
Tractor Nguyen <thonguyen4295@gmail.com>
2021-07-17 22:35:01 +0700

    update4

--- Ex-1.c
+++ Ex-1.c
@@ -1,6 +1,15 @@
+//Bài 1: Tính S(n) = 1 + 2 + 3 + … + n
 #include<stdio.h>
 
 int main(){
-    printf("Hello Cc");
+    int sum = 0;
+    int n;
+    printf("Enter n = ");
+    scanf("%d",&n);
+    for(int i = 0; i<=n; ++i)
+    {
+        sum +=i;
+    }
+    printf("Sum = %d",sum);
     return 0;
 }
