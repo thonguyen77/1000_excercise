69b16c6b5da36436a8f23ab934f51fa9ca3f5867
Tractor Nguyen <thonguyen4295@gmail.com>
2021-07-17 22:51:01 +0700

    update5

--- Ex-2.c
+++ Ex-2.c
@@ -1,6 +1,16 @@
+//Bài 2: Tính S(n) = 1^2 + 2^2 + … + n^2
 #include<stdio.h>
-
+#include<math.h>
 int main()
 {
+    double sum = 0;
+    double n;
+    printf("Enter n = ");
+    scanf("%lf",&n);
+    for(double i = 1; i<=n; ++i)
+    {
+        sum += pow(i,2);
+    }
+    printf("Sum = %lf",sum);
     return 0;
 }
